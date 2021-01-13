95d8d293c4877614d7b2ec599663d632e49f421b
fkuca <fkuca91@gmail.com>
2021-01-13 17:05:14 +0100

    Add server template code and cleanup README.md content

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const http = require("http");
+
+const server = http.createServer(function (req, res) {
+  console.log(`Request came from ${req.url}`);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Plain text!");
+});
+
+server.listen(3000, "127.0.0.1");
+console.log("Server in listening mode ...");
